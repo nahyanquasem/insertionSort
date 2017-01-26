5870a6db8fec5faac2878ed0cf1dd291d7e34e81
tinman072 <nahyan.quasem+github@gmail.com>
2017-01-26 14:00:20 +0600

    Add files via upload

---
+++ insertionSort/insertionSort/Module1.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim list() As Integer = {23, 19, 17, 13, 11, 7, 5, 3, 2}
+
+        Dim i, j, key As Integer
+
+        i = 0
+        j = 1
+
+        For j = 1 To 8
+            key = list(j)
+
+            i = j - 1
+
+            While i >= 0
+                If list(i) > key Then
+                    list(i + 1) = list(i)
+                    i = i - 1
+                End If
+
+            End While
+
+            list(i + 1) = key
+
+        Next
+
+        For i = 1 To 8
+            Console.Write(list(i) & " ")
+        Next
+
+
+
+    End Sub
+
+End Module
---
+++ insertionSort/insertionSort/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("insertionSort")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("insertionSort")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c07cd9f5-5262-49bb-932e-27727cd137aa")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
